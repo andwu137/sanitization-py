211c20316ceb0d01b0a7cae342928b9fed0d77fc
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-06-13 14:08:18 -0400

    Temp: begin spotter class method definitions, finish init

--- src/sanitize.py
+++ src/sanitize.py
@@ -1,3 +1,40 @@
+# External Imports
+from typing import Optional
+from collections import namedtuple
+import re
+
+# Internal Imports
+
+
+# Classes
+Regex = namedtuple("Regex", ["pattern", "type"])
+
+
+class Spotter:
+    defaultRegexes: dict[str, Regex] = {}
+
+    def __init__(self, regexes: Optional[dict[str, Regex]] = None):
+        self.regexes: dict[str, Regex] = regexes or self.defaultRegexes
+        self.regexes = {
+            name: Regex(re.compile(regex.pattern, re.IGNORECASE), regex.type)
+            for name, regex in self.regexes.items()
+        }
+
+    @staticmethod
+    def runRegex(reg, line) -> str:
+        raise NotImplementedError()
+        if reg.type == "":
+            return reg.pattern.sub("", line)
+        return ""
+
+    def process_line(self, line: str) -> str:
+        raise NotImplementedError()
+        for id, regex in self.regexes.items():
+            line = self.runRegex(regex, line)
+        return line
+
+
+# Functions
 def main():
     pass
 
