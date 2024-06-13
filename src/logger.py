1a064767dc3958e610f55ab35bd07e28452b7f54
Rom0 <80369669+Rom0@users.noreply.github.com>
2024-06-13 13:19:30 -0400

    Added existing logger function

---
+++ src/logger.py
@@ -0,0 +1,32 @@
+import datetime
+
+class Logger:
+    file_path = ""
+
+    @staticmethod
+    def log(msg):
+        with open(Logger.file_path, "w") as file:
+            file.write(f"{datetime.now()} - {msg}")
+        file.close()
+    
+    @staticmethod
+    def log_start(obj, **args):
+        string = f"Began process {obj.getSpotterUID()} on file blank\n"
+        Logger.log(string)
+
+    @staticmethod
+    def log_removal(obj, data):
+        #DATETIME SPOTTERUID REMOVED THIS STRING: ------
+        string = f"Spotter {obj.getSpotterUID()} removed {data}\n"
+        Logger.log(string)
+    
+    @staticmethod
+    def log_end(obj, num):
+        string = f"Spotter {obj.getSpotterUID()} ended process with {num} removals.\n"
+        Logger.log(string)
+        
+    @staticmethod
+    def log_error(obj, error, msg):
+        string = f"{error} - Spotter {obj.getSpotterUID()}: {msg} \n"
+        Logger.log(string)
+        
