cdead28b037a6d0aaa7300148597e4abc33cd28a
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-06-13 13:26:44 -0400

    lint, and fix method missing error

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import datetime
+
 
 class Logger:
     file_path = ""
@@ -8,7 +9,7 @@ class Logger:
         with open(Logger.file_path, "w") as file:
             file.write(f"{datetime.now()} - {msg}")
         file.close()
-    
+
     @staticmethod
     def log_start(obj, **args):
         string = f"Began process {obj.getSpotterUID()} on file blank\n"
@@ -16,17 +17,17 @@ class Logger:
 
     @staticmethod
     def log_removal(obj, data):
-        #DATETIME SPOTTERUID REMOVED THIS STRING: ------
+        # DATETIME SPOTTERUID REMOVED THIS STRING: ------
         string = f"Spotter {obj.getSpotterUID()} removed {data}\n"
         Logger.log(string)
-    
+
     @staticmethod
     def log_end(obj, num):
         string = f"Spotter {obj.getSpotterUID()} ended process with {num} removals.\n"
         Logger.log(string)
-        
+
     @staticmethod
     def log_error(obj, error, msg):
         string = f"{error} - Spotter {obj.getSpotterUID()}: {msg} \n"
         Logger.log(string)
-        
+
